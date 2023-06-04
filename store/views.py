70be4296e44589377869c3550891b12680e93379
vinayak <vatsvinayak2000@gmail.com>
2023-06-04 11:35:02 +0530

    Added Product page, product config and category config

---
+++ store/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+# Create your views here.
+def store(request, category_slug=None):
+    categories=None
+    products=None
+
+    if category_slug!=None:
+        categories=get_object_or_404()
+    products = Product.objects.all().filter(is_available=True)
+    product_count=products.count()
+    context={
+        'products': products,
+        "product_count": product_count,
+    }
+    return render(request,'store/store.html', context )
